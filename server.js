6c65725672620ef9407dc26817ae3844a62a61e6
4ions <leovalenciasantana@gmail.com>
2021-09-02 22:47:05 -0500

    Installed morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//const logger = require('./middleware/logger');
+const morgan = require('morgan');
 
 //Rooute Files
 const bootcamps = require('./routes/bootcamps');
@@ -9,6 +11,13 @@ dotenv.config({ path: './config/config.env' });
 
 const app = express();
 
+//dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+//app.use(logger);
 
 //mount routers
 app.use('/api/v1/bootcamps', bootcamps);
